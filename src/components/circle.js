5308749f3379c8d079b0cc18800f0c455178f8e9
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-10-19 20:46:21 +0530

    change

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,7 +1,11 @@
-// import 
+import Shape from "./shape";
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+  calculateArea = function () {};
+}
 
 // declare class
 
-
 // export
-
